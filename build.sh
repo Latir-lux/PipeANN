e2d6de47d65fb4e04ab1b7f5853c2cfa33acc918
Latir <latiring@gmail.com>
2025-12-04 10:16:39 +0800

    lab环境修改

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 mkdir build
 cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+cmake .. \
+	-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+	-DCMAKE_C_COMPILER=/home/xiaoxuanx/bin/gcc \
+	-DCMAKE_CXX_COMPILER=/home/xiaoxuanx/bin/g++
 make -j

5fc1059b498dad2aa3cbc367e49d1363e6a1325c
Latir <latiring@gmail.com>
2026-01-22 17:17:47 +0800

    fix libaio

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-mkdir build
+mkdir -p build
 cd build
 cmake .. \
+	-DUSE_AIO=ON \
 	-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
 	-DCMAKE_C_COMPILER=/home/xiaoxuanx/bin/gcc \
 	-DCMAKE_CXX_COMPILER=/home/xiaoxuanx/bin/g++
